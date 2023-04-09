cddfaa067c3573f3c4f07139ff95ae3cbe1d4e2d
Ruzan Valeev <ruzan.valeeff@yandex.ru>
2023-04-09 21:52:43 +0300

    Решение задачи 18 из файла, обязательно прочитайте Readme.txt хотя бы до слов "Объяснение:"

---
+++ LaplaceEquation/GridCalculator.cs
@@ -0,0 +1,75 @@
+﻿namespace LaplaceEquation;
+
+public class GridCalculator
+{
+
+    /// <summary>
+    /// Что приближать, и до какого знака после запятой
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="accuracy"></param>
+    /// <returns></returns>
+    public double[,] Calculate(double[,] matrix, int accuracy)
+    {
+        ThreadPool.GetMinThreads(out int threadsCount, out _);
+        int width = matrix.GetLength(0), height = matrix.GetLength(1);
+
+        var matrixPartWidth = (int)Math.Max(1, Math.Ceiling((double)width / threadsCount));
+
+        for(int i = 0; i < threadsCount; i++)
+        {
+            var i1 = i * matrixPartWidth;
+            
+            if (i1 < width)
+                new Thread(() => PartUpdate(i1, matrixPartWidth)).Start();
+            
+            else break;
+        }
+
+        while (!IsAccuracy()) ;
+
+        return matrix;
+
+        void PartUpdate(int startIndex, int length)
+        {
+            var endIndex = startIndex + length;
+
+            while (!IsAccuracy())
+            {
+                for (int i = startIndex; i < endIndex; i++)
+                {
+                    // Границы нельзя менять по идее. Иначе, в конечном итоге, мы получим просто плоскость
+                    // (если программа будет достаточно долго работать)
+                    if (i != 0 && i != width - 1)
+                    {
+                        for (int j = 1; j < height - 1; j++)
+                        {
+                            matrix[i, j] = Math.Round(GetNeighborAverage(i, j), accuracy);
+                        }
+                    }
+                }
+            }
+        }
+    
+        bool IsAccuracy()
+        {
+            for (int i = 1; i < width - 1; i++)
+            {
+                for (int j = 1; j < height - 1; j++)
+                {
+                    var current = matrix[i, j]; 
+                    var expected = GetNeighborAverage(i, j);
+
+                    if (Math.Abs(expected - current) > Math.Pow(0.1, accuracy))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        double GetNeighborAverage(int i, int j)
+        {
+            return (matrix[i - 1, j] + matrix[i, j - 1] + matrix[i + 1, j] + matrix[i, j + 1]) / 4;
+        }
+    }
+}
---
+++ LaplaceEquation/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LaplaceEquation
+{
+    internal class Program
+    {
+        // Задача номер 18 из файла
+        static void Main()
+        {   
+            int w = 10, h = 20, accuracy = 8;
+            var matrix = new double[w, h];
+
+            #region Matrix fill
+            for (int i = 0; i < w; i++)
+            {
+                matrix[i, 0] = i;
+            }
+            
+            for (int i = 0; i < h; i++)
+            {
+                matrix[0, i] = i;
+            }
+
+            for (int i = w - 1; i >= w - h; i--)
+            {
+                matrix[w - 1, (w - 1) - i] = i;
+            }
+
+            for (int i = 1; i < w - 1; i++)
+            {
+                matrix[i, h - 1] = Math.Round(matrix[0, h - 1] - i * ((matrix[0, h - 1] - matrix[w - 1, h - 1]) / w), 3);
+            }
+            #endregion Matrix fill
+            PrintMatrix(matrix);
+
+            var approximatedMatrix = new GridCalculator().Calculate(matrix, accuracy);
+            PrintMatrix(approximatedMatrix);
+        }
+
+        static void PrintMatrix(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
